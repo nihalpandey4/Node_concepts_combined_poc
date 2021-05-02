6ceb18fe72304bad7d24929fd92db436b94ebd88
Nihal Pandey <nihalpandey@farmart.co>
2021-05-02 14:06:32 +0530

    sign_in route added

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const sign_up_controller = require("./controllers/sign_up");
+const sign_in_controller = require("./controllers/sign_in");
 
 router.post("/sign_up",sign_up_controller);
+router.post ("/sign_in",sign_in_controller);
 
 module.exports = router;
