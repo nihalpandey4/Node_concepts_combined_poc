e89180a78406ffa264409fddb244697b49ffe29a
Nihal Pandey <nihalpandey@farmart.co>
2021-05-02 14:09:38 +0530

    db connection close added in graceful shutdown

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const app_routes = require("./routes");
 const db_generator = require("./database/db_generator");
+const db_connection_initializer = require("./database/db_connection_initializer")
 
 app.use(logger("dev"));
 
@@ -21,6 +22,7 @@ const server = app.listen(3000,()=>console.log("at 3000"))
 process.on("SIGINT",()=>{
     console.log("SIGINT signal received");
     server.close(()=>{
+        db_connection_initializer.close();
         console.log("Closing express server for new incoming requests");
     })
 })
